a6f125259e3ad061d8f3612703b84559e1accbd1
João Afonso Katombela <ja3328173@gmail.com>
2023-11-03 16:38:27 +0100

    p

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -1,46 +1,60 @@
 import React from 'react'
 import { BsFacebook } from 'react-icons/bs';
-import {BsWhatsapp } from 'react-icons/bs';
-import {BsInstagram} from 'react-icons/bs';
-import {BsTwitter} from 'react-icons/bs';
+import { BsWhatsapp } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { BsTwitter } from 'react-icons/bs';
 import './footer.css'
 
 const Footer = () => {
     return (
         <>
-        <div className='footer bg-primary'>
-            <div className='container'>
-                <div className='sobre'>
-                    <div className='logo'>
-                        <img src='.\img\logo.jpg ' alt='logo'></img>
+            <div className='foot '>
+                <div className='container py-5 row'>
+
+                    <div className='col-12 my-2 text-start px-4 container col-md-3'>
+                       
+                            <img src='.\img\logo.png ' alt='logo' className='logo-f'></img>
+                        
+                        <div className='icone'>
+                            <ul>
+                                <li><BsFacebook /></li>
+                                <li><BsWhatsapp /></li>
+                                <li><BsInstagram /></li>
+                                <li><BsTwitter /></li>
+                            </ul>
+                        </div>
                     </div>
-                   <div className='icone'>
-                        <li><BsFacebook/></li>
-                        <li><BsWhatsapp/></li>
-                        <li><BsInstagram/></li>
-                        <li><BsTwitter/></li>
+
+                    <div className='col-12 my-2 c px-4 col-md-3  '>
+                        <h3>Minha Conta</h3>
+                        <ul>
+                            <li>Login</li>
+                            <li>Cadastro</li>
+                            <li>Carrinho</li>
+                        </ul>
+                    </div>
+
+                    <div className='col-12 my-2 c col-md-3 px-3  '>
+                        <h3>Links Rápidos</h3>
+                        <ul>
+                            <li>Pagina Inicial</li>
+                            <li>Producto</li>
+                            <li>Sobre</li>
+                            <li>Contacto</li>
+                        </ul>
+                    </div>
+                    <div className='col-12 my-2 c col-md-3 px-3  '>
+                        <h3>Legal</h3>
+                        <ul>
+                            <li>Políticas de reembolso</li>
+                            <li>Entregas</li>
+                            <li>Locais de Recolhas</li>
+                            <li>Politicas e Termos</li>
+                        </ul>
                     </div>
                 </div>
-          
-           <div className='conta'>
-            <h3>Minha Conta</h3>
-            <ul>
-                <li>Login</li>  
-                <li>Cadastro</li>
-                <li>Carrinho</li>
-            </ul>
             </div>
-           <div className= 'pagina'>
-            <h3>Paginas</h3>
-            <ul>
-                 <li>Pagina Inicial</li>
-                    <li>Producto</li>
-                    <li>Sobre</li>
-                    <li>Contacto</li>
-            </ul>
-        </div>
-             </div>
-        </div>
+            <center className="bg-light copy">&copy; 2023 &middot; IO &middot; Desenvolvido por <a href="http://www.arotec.ao">AROTEC</a></center>
         </>
     )
 }
--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -6,7 +6,7 @@ import { MdCardMembership } from 'react-icons/md';
 import { TfiHeadphoneAlt } from 'react-icons/tfi';
 import { PiShoppingCartBold } from 'react-icons/pi';
 import { ImEye } from 'react-icons/im';
-import promoimg from '../img/smart watch.png'
+import promoimg from '../img/ip2.jpg'
 import { AiOutlineHeart, AiOutlineCloseCircle } from 'react-icons/ai';
 import Homeproduto from '../homeproduto';
 import './estilos/home.css'
@@ -23,40 +23,40 @@ const Home = ({ detalhe, ver, fechar, setFechar, addcarrinho }) => {
                                 detalhe.map((curElm) => {
                                     return (
                                         <div className='boxproduto py-5'>
-                                        <div className="row w-100 my-3">
-                                          <div className=' text-center col-12 col-md-4 col-xl-5 '>
-                                            <img src={curElm.Img} alt={curElm.Titulo} className="w-100 my-auto" />
-                                          </div>
-                    
-                                          <div className='detalhe px-4 px-md-0 col-12 col-md-8 col-xl-7 '>
-                                            <h1>{curElm.Titulo}</h1>
-                                            <h4 className="text-secondary">{curElm.Cat}</h4>
-                                            <p className="d-flex gap-2">
-                                              <BsStarFill className="text-warning"/>
-                                              <BsStarFill className="text-warning"/>
-                                              <BsStarFill className="text-warning"/>
-                                              <BsStarHalf className="text-warning"/>
-                                              <BsStar className="text-warning"/>
-                                            </p>
-                                            <h3>{curElm.Preco} Kz</h3>
-                                            <p className="text-secondary">
-                                              Telefone recondicionado, vindo do lugar x ou algo parecido a isto, sei lá
-                                            </p>
-                                            <div className="d-flex gap-4 btns-buy flex-wrap ">
-                                              <button onClick={() => addcarrinho(curElm)} className="w-sm-100">Adicionar ao carrinho <BsCartPlus /></button>
-                    
-                                              <button className="w-sm-100">Comprar agora <BsCart2 /></button>
+                                            <div className="row w-100 my-3">
+                                                <div className=' text-center col-12 col-md-4 col-xl-5 '>
+                                                    <img src={curElm.Img} alt={curElm.Titulo} className="w-100 my-auto" />
+                                                </div>
+
+                                                <div className='detalhe px-4 px-md-0 col-12 col-md-8 col-xl-7 '>
+                                                    <h1>{curElm.Titulo}</h1>
+                                                    <h4 className="text-secondary">{curElm.Cat}</h4>
+                                                    <p className="d-flex gap-2">
+                                                        <BsStarFill className="text-warning" />
+                                                        <BsStarFill className="text-warning" />
+                                                        <BsStarFill className="text-warning" />
+                                                        <BsStarHalf className="text-warning" />
+                                                        <BsStar className="text-warning" />
+                                                    </p>
+                                                    <h3>{curElm.Preco} Kz</h3>
+                                                    <p className="text-secondary">
+                                                        Telefone recondicionado, vindo do lugar x ou algo parecido a isto, sei lá
+                                                    </p>
+                                                    <div className="d-flex gap-4 btns-buy flex-wrap ">
+                                                        <button onClick={() => addcarrinho(curElm)} className="w-sm-100">Adicionar ao carrinho <BsCartPlus /></button>
+
+                                                        <button className="w-sm-100">Comprar agora <BsCart2 /></button>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12 px-4 px-md-5 mt-md-5 container mt-4">
+
+                                                    <h2> Sobre este produto</h2>
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere recusandae nobis modi laudantium temporibus ut tempora beatae doloribus perspiciatis sapiente quae, omnis a, accusantium reprehenderit excepturi quas odit magni? Totam.
+                                                    </p>
+                                                </div>
                                             </div>
-                                          </div>
-                                          <div className="col-12 px-4 px-md-5 mt-md-5 container mt-4">
-                    
-                                            <h2> Sobre este produto</h2>
-                                            <p>
-                                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere recusandae nobis modi laudantium temporibus ut tempora beatae doloribus perspiciatis sapiente quae, omnis a, accusantium reprehenderit excepturi quas odit magni? Totam.
-                                            </p>
-                                          </div>
                                         </div>
-                                      </div>
                                     )
                                 })
                             }
@@ -98,16 +98,17 @@ const Home = ({ detalhe, ver, fechar, setFechar, addcarrinho }) => {
             <hr />
             */}
             <div className="produto container">
-                <center>
+                <br />
+                {/*<center>
                     <h1 className="titulo">Explore Nossa Loja</h1>
-                </center>
+                </center>*/}
                 <div className="container-flui ">
                     <div className="row ">
                         {
-                            Homeproduto.map((curElm) => {
+                            Homeproduto.slice(0, 8).map((curElm) => {
                                 return (
                                     <div className=" col-12 col-sm-6 col-md-4 col-lg-3  col-xxl-3" key={curElm.id}>
-                                        <div  onClick={() => ver(curElm)} className="box">
+                                        <div title={`Clique para ver ` + curElm.Titulo} className="box">
                                             <div className="img_box">
                                                 <img src={curElm.Img} alt={curElm.Titulo} />
                                                 <div className="icone">
@@ -116,7 +117,7 @@ const Home = ({ detalhe, ver, fechar, setFechar, addcarrinho }) => {
                                                     <li> <AiOutlineHeart /></li>
                                                 </div>
                                             </div>
-                                            <div className="detalhe">
+                                            <div onClick={() => ver(curElm)} className="detalhe">
                                                 <p>{curElm.Cat}</p>
                                                 <h3>{curElm.Titulo}</h3>
                                                 <h4>{curElm.Preco} kz</h4>
@@ -129,20 +130,54 @@ const Home = ({ detalhe, ver, fechar, setFechar, addcarrinho }) => {
                     </div>
                 </div>
             </div>
-            <div className="banner container">
+            <div className="banner mt-2 px-2 mx-0 mx-sm-auto container">
                 <div className="containe row">
                     <div className="detalhe text-center text-start-md  text-md-start col-12 col-md-7">
-                        <div className="my-auto mt-5">
+                        <div className="my-auto mt-4 mt-md-5">
                             <h6>O produto mais recente</h6>
-                            <h3>Apple Watch Pro Max</h3>
-                            <p>70.000.00Kz</p>
-                            <button>
-                                <Link to='/produto' className='link'>Compre agora</Link>
-                            </button>
+                            <h3>Iphone 12 Pro Ma Pro Max</h3>
+                            <p>170.000.00Kz</p>
+                                <Link to='/produto' className='link bg-dark btn btn-dark'>Compre agora</Link>
+                         
                         </div>
                     </div>
-                    <div className="img_box col-12  pt-md-0 py-5 text-center text-start-md  text-md-start col-md-5">
-                        <img src={promoimg} alt="promo"></img>
+                    <div className="img_box col-12  pt-md-0 py-4 py-md-3 text-center text-start-md  text-md-start col-md-5">
+                        <img src={promoimg} className="rounded-2" alt="promo"></img>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="produto mt-3 mb-5 container">
+                <br />
+                <h2>Mais Produtos</h2 >
+                {/*<center>
+                    <h1 className="titulo">Explore Nossa Loja</h1>
+                </center>*/}
+                <div className="container-flui ">
+                    <div className="row ">
+                        {
+                            Homeproduto.slice(7).map((curElm) => {
+                                return (
+                                    <div className=" col-12 col-sm-6 col-md-4 col-lg-3  col-xxl-3" key={curElm.id}>
+                                        <div title={`Clique para ver `+curElm.Titulo} className="box">
+                                            <div className="img_box">
+                                                <img src={curElm.Img} alt={curElm.Titulo} />
+                                                <div className="icone">
+                                                    <li onClick={() => addcarrinho(curElm)}> <PiShoppingCartBold /></li>
+                                                    <li onClick={() => ver(curElm)}><ImEye /></li>
+                                                    <li> <AiOutlineHeart /></li>
+                                                </div>
+                                            </div>
+                                            <div onClick={() => ver(curElm)} className="detalhe d-t">
+                                                <p>{curElm.Cat}</p>
+                                                <h3>{curElm.Titulo}</h3>
+                                                <h4>{curElm.Preco} kz</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
