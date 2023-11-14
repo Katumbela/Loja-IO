6dd897dd3120150f00c9a13b68472f994d655042
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-11-14 18:22:20 +0100

    installing firebase

--- src/paginas/resumo.jsx
+++ src/paginas/resumo.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { db } from "./config";
 
 const ResumoCompra = ({ metodoObtencao, metodoPagamento, informacoesEntrega, finalizarCompra }) => {
-  return (
+  
+    // Exemplo de como salvar dados no Firestore
+const salvarDadosFirestore = (metodoObtencao, metodoPagamento, informacoesEntrega) => {
+    db.collection("compras").add({
+      metodoObtencao,
+      metodoPagamento,
+      informacoesEntrega,
+      dataCompra: new Date(),
+    })
+    .then((docRef) => {
+      console.log("Compra salva com ID:", docRef.id);
+      // Aqui você pode chamar a função para gerar o PDF com os dados
+    })
+    .catch((error) => {
+      console.error("Erro ao salvar a compra: ", error);
+    });
+  };
+  
+  
+    return (
     <div>
 
       <h4>Resumo da Compra</h4>
-      
+
       <p>Método de Obtenção: {metodoObtencao}</p>
       <p>Método de Pagamento: {metodoPagamento}</p>
       <p>Informações de Entrega:</p>
